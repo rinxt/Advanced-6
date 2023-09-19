64d09d4e8fdab9814cceee90e809beb26f5b4f3f
Мамаева Арина Евгеньевна <m18282@kometadom.local>
2023-09-19 15:13:33 +0300

    Task2 added

---
+++ Task2/Task2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <set>
+
+int main()
+{
+    std::set<int> numbers;
+
+    int numbersCount;
+    std::cin >> numbersCount;
+
+    for (int i = 0; i < numbersCount; i++) {
+        int num;
+        std::cin >> num;
+        numbers.insert(num);
+    }
+
+    for (auto it = numbers.rbegin(); it != numbers.rend(); ++it) {
+        std::cout << *it << " ";
+    }
+}
+
